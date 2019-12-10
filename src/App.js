7fd3d777f9501d23c16d705b5c5821a1bd6eb644
Josh Marinacci <joshua@marinacci.org>
2019-12-10 12:21:41 -0800

    make drawing work again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
+import React, {Component, useState} from 'react'
 import './App.css';
+import {hsvToCanvas, randRange, randSpread, sfc32, toRad, xmur3} from './utils.js'
 
 
 
@@ -22,11 +23,6 @@ let mainDoc = {
       _type:'fixed',
       value:70,
     },
-    color: {
-      _title:'Hue',
-      _type:'fixed',
-      value:0.1
-    },
     type: {
       _title:'Shape',
       _type:'pick',
@@ -45,7 +41,7 @@ let mainDoc = {
     size: {
       _title:'Size',
       _type: 'spread',
-      base:'20',
+      base:20,
       spread:3
     },
     type: {
@@ -53,13 +49,6 @@ let mainDoc = {
       _type: 'pick',
       value:'square',
       values:['square','circle','triangle','ellipse'],
-    },
-    color: {
-      _title:'Hue',
-      _type:'fixed',
-      _kind:'number',
-      value:0.5,
-      _step:0.1,
     },
     angle: {
       _title:'Angle',
@@ -72,8 +61,8 @@ let mainDoc = {
     _title: 'Branches',
     angle: {
       _type:'spread',
-      base:'25',
-      spread:'8'
+      base:25,
+      spread:8
     }
   },
 }
@@ -100,14 +89,14 @@ const RandomSpreadEditor = ({def,update}) => {
     <label className='sub'>base</label>
     <input type='number' value={def.base}
            onChange={(e)=>{
-             def.base = e.target.value
+             def.base = parseFloat(e.target.value)
              update()
            }}
     />
     <label className='sub'>spread</label>
     <input type='number' value={def.spread}
            onChange={(e)=>{
-             def.spread = e.target.value
+             def.spread = parseFloat(e.target.value)
              update()
            }}
     />
@@ -117,7 +106,10 @@ const RandomSpreadEditor = ({def,update}) => {
 const PickEditor = ({def,update}) => {
   return <HBox>
     <label className='title'>{def._title}</label>
-    <select>
+    <select value={def.value} onChange={(e)=>{
+      def.value = e.target.value
+      update()
+    }}>
       {def.values.map(val => {
         return <option value={val} key={val}>{val}</option>
       })}
@@ -150,14 +142,154 @@ const DocEditor = ({doc,update}) => {
   </div>
 }
 
+const PHASES = {
+  TRUNK:'TRUNK',
+  LEAF:'LEAF'
+}
+
+class CanvasView extends Component {
+  componentDidMount() {
+    this.redraw()
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.redraw()
+  }
+  render() {
+    return <canvas ref={(canvas)=>this.canvas = canvas} width={300} height={300}/>
+  }
+
+  redraw() {
+    const ctx = this.canvas.getContext('2d')
+    this.ctx = ctx
+    const w = this.canvas.width
+    const h = this.canvas.height
+    //background
+    ctx.fillStyle = '#cccccc'
+    ctx.fillRect(0,0,w,h)
+
+    //floor
+    ctx.fillStyle = '#d0d0d0'
+    ctx.fillRect(0,h-10,w,10)
+    this.props.doc.seed = 'apple';//""+Math.random()
+
+    // draw the two phases
+    this.firstTrunk(PHASES.TRUNK,this.props.doc)
+    this.firstTrunk(PHASES.LEAF,this.props.doc)
+  }
+
+  firstTrunk(phase,opts) {
+    const w = this.canvas.width
+    const h = this.canvas.height
+    this.phase = phase
+    this.ctx.save()
+    this.ctx.translate(w/2,h-10)
+    this.ctx.scale(1,-1)
+    const seed = xmur3(opts.seed)
+    opts.random =sfc32(seed(),seed(),seed(),seed())
+    let width = opts.trunk.width.value
+    let height = opts.trunk.height.value
+
+    // opts.trunk.color.value = randRange(opts.random,0,1)
+    // opts.leaf.color.value = 1.0-opts.trunk.color.value
+    // if(opts.leaf.color.value < 0) opts.leaf.color.value = opts.leaf.color.value + 1.0
+    const o2 = {
+      trunkColor: randRange(opts.random,0,1),
+    }
+    o2.leafColor = 1.0-o2.trunkColor
+    if(o2.leafColor < 0) o2.leafColor = o2.leafColor + 1.0
+    this.trunk(this.ctx, width, height, opts.maxDepth.value, opts, o2)
+    this.ctx.restore()
+
+  }
+
+  branch(ctx, w, h,depth, opts,o2) {
+    const angle = toRad(opts.branch.angle.base)
+    const spread = toRad(opts.branch.angle.spread)
+    ctx.save()
+    ctx.rotate(randSpread(opts.random,+angle,spread))
+    this.trunk(ctx,w,h,depth-1, opts,o2)
+    ctx.restore()
+    ctx.save()
+    ctx.rotate(randSpread(opts.random,-angle,spread))
+    this.trunk(ctx,w,h,depth-1, opts,o2)
+    ctx.restore()
+  }
+
+  trunk(ctx, w, h, depth, opts, o2) {
+      const nw = w * opts.trunk.attenuation.base
+      const nh = h * randSpread(opts.random,opts.trunk.attenuation.base,opts.trunk.attenuation.spread)
+
+      if(this.phase === PHASES.TRUNK) {
+        const step = 0.5/opts.maxDepth.value
+        ctx.fillStyle = hsvToCanvas(o2.trunkColor,0.8,1.0-depth*step)
+        if(opts.trunk.type.value === 'rectangle') {
+          ctx.fillRect(-w / 2, 0, w, nh)
+        }
+        if(opts.trunk.type.value === 'trapezoid') {
+          ctx.beginPath()
+          ctx.moveTo(-w/2,0)
+          ctx.lineTo(-nw/2,nh)
+          ctx.lineTo(nw/2,nh)
+          ctx.lineTo(w/2,0)
+          ctx.fill()
+        }
+      }
+      ctx.save()
+      ctx.translate(0,nh)
+      if(depth === 0) this.leaf(ctx,nw, nh,opts,o2)
+      if(depth > 0) this.branch(ctx,nw,nh,depth, opts,o2)
+      ctx.restore()
+  }
+
+  leaf(ctx, w,h, opts,o2) {
+    ctx.save()
+    const angle = randSpread(opts.random,opts.leaf.angle.base, opts.leaf.angle.spread)
+    ctx.rotate(toRad(angle))
+    const sat = randSpread(opts.random,0.5,0.2)
+    ctx.fillStyle = hsvToCanvas(o2.leafColor,sat,1.0)
+    let s = randSpread(opts.random,opts.leaf.size.base,opts.leaf.size.spread)
+    if(s <0) s = 0
+
+    if(this.phase !== PHASES.LEAF) {
+      return ctx.restore()
+    }
+    if(opts.leaf.type.value === 'square') {
+      ctx.fillRect(-s, -s, s * 2, s * 2)
+    }
+    if(opts.leaf.type.value === 'circle') {
+      ctx.beginPath()
+      ctx.arc(0,0,s,0,toRad(360))
+      ctx.fill()
+    }
+    if(opts.leaf.type.value === 'triangle') {
+      ctx.beginPath()
+      for(let i=0; i<=3; i++) {
+        let theta = toRad(360/3*i)
+        let x = s*Math.sin(theta)
+        let y = s*Math.cos(theta)
+        if(i === 0) ctx.moveTo(x,y)
+        ctx.lineTo(x,y)
+      }
+      ctx.fill()
+    }
+    if(opts.leaf.type.value === 'ellipse') {
+      ctx.beginPath()
+      ctx.ellipse(0,0,s,s/2,0,0,toRad(360))
+      ctx.fill()
+    }
+    ctx.restore()
+  }
+
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
   return (
-    <div>
-      <DocEditor doc={mainDoc} update={()=>{
-        setCount(count+1)
-      }}/>
-    </div>
+    <HBox>
+      <DocEditor doc={mainDoc} update={()=> setCount(count+1)}/>
+      <CanvasView doc={mainDoc}/>
+    </HBox>
   );
 }
 
---
+++ src/utils.js
@@ -0,0 +1,67 @@
+export function xmur3(str) {
+    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+        h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+        h = h << 13 | h >>> 19;
+    }
+    return function() {
+        h = Math.imul(h ^ h >>> 16, 2246822507);
+        h = Math.imul(h ^ h >>> 13, 3266489909);
+        return (h ^= h >>> 16) >>> 0;
+    }
+}
+export function sfc32(a, b, c, d) {
+    return function() {
+        a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0;
+        let t = (a + b) | 0;
+        a = b ^ b >>> 9;
+        b = c + (c << 3) | 0;
+        c = (c << 21 | c >>> 11);
+        d = d + 1 | 0;
+        t = t + d | 0;
+        c = c + t | 0;
+        return (t >>> 0) / 4294967296;
+    }
+}
+export const toRad = (deg) => deg/180*Math.PI
+export const randRange = (random, min, max) => min + random()*(max-min)
+export const randSpread = (random, base, spread) => randRange(random,base-spread,base+spread)
+
+// https://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+export function hsvToRgb(h, s, v){
+    let r, g, b
+
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+    }
+
+    return [r * 255, g * 255, b * 255];
+}
+
+export function hsvToCanvas(h, s, v) {
+    const rgb = hsvToRgb(h,s,v)
+    return `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
+}
+
+
